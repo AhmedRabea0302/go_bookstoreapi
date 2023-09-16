8aa0b6395520c72d7fa041878a9099e10d2c43fa
AhmedRabea0302 <arabea169@gmail.com>
2023-09-16 21:41:00 +0300

    Created DeleteBook And UpdateBook funcs in book controller

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/AhmedRabea0302/go_bookstoreapi/pkg/models"
+	"github.com/AhmedRabea0302/go_bookstoreapi/pkg/utils"
 	"github.com/gorilla/mux"
 )
 
@@ -32,6 +33,58 @@ func GetBookById(w http.ResponseWriter, r *http.Request) {
 
 	res, _ := json.Marshal(bookDetails)
 	w.Header().Set("Content-Type", "application")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(res)
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	bookId := params["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error parsing bookId")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	var updateBook models.Book
+	utils.ParseBody(r, updateBook)
+	params := mux.Vars(r)
+	bookId := params["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("Error parsing bookId")
+	}
+
+	bookDetails, db := models.GetBookById(ID)
+	if updateBook.Name != "" {
+		bookDetails.Name = updateBook.Name
+	}
+	if updateBook.Author != "" {
+		bookDetails.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		bookDetails.Publication = updateBook.Publication
+	}
+	db.Save(&bookDetails)
+	res, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
